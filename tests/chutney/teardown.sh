249d2466038ecc921fecf47131913583185fccf8
Trinity Pointard <trinity.pointard@gmail.com>
2021-11-22 18:57:08 +0100

    Revert "add optional coverage on integration test"

--- tests/chutney/teardown.sh
+++ tests/chutney/teardown.sh
@@ -13,10 +13,4 @@ tail --pid="$pid" -f /dev/null
 ./chutney/chutney stop "$target"
 
 source tests/chutney/arti.run
-if [ "$result" != 0 ]; then
-	exit "$result"
-fi
-
-if [ "${COVERAGE}" == 1 ]; then
-	grcov coverage_meta/ --binary-path target/debug/ -s crates/ -t html --branch --ignore-not-existing -o coverage/
-fi
+exit "$result"
