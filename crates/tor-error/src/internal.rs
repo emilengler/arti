a7a045f4c0016d9288f29c0b1970590676ac3d74
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-05-13 16:00:49 +0100

    Fix typos in comments

--- crates/tor-error/src/internal.rs
+++ crates/tor-error/src/internal.rs
@@ -250,10 +250,10 @@ impl HasKind for Bug {
 mod test {
     use super::*;
 
-    // We tet this on Linux only.  This test case is to ensure that we are using the
+    // We test this on Linux only.  This test case is to ensure that we are using the
     // backtrace module correctly, etc., which can be checked by doing it on one platform.
     // Doing it on others simply exposes us to the vagaries of platform backtrace support.
-    // Arti ought not to fail its tests just because someone is using a platform with poor.
+    // Arti ought not to fail its tests just because someone is using a platform with poor
     // backtrace support.
     #[cfg(target_os = "linux")]
     #[test]
