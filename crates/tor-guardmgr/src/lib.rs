77199a0ffa4407ff7613ddc80fd9dc33a66fb166
Ian Jackson <ijackson@chiark.greenend.org.uk>
2022-06-17 14:31:28 +0100

    list builder accessor docs: Move in tor-guardmgr::GuardUsage

--- crates/tor-guardmgr/src/lib.rs
+++ crates/tor-guardmgr/src/lib.rs
@@ -1357,9 +1357,7 @@ pub struct GuardUsage {
     /// The purpose for which this guard will be used.
     #[builder(default)]
     kind: GuardUsageKind,
-    /// A list of restrictions on which guard may be used.
-    ///
-    /// The default is the empty list.
+    /// See accessor docs
     #[builder(sub_builder, setter(custom))]
     restrictions: GuardRestrictionList,
 }
@@ -1380,6 +1378,9 @@ define_list_builder_helper! {
 
 define_list_builder_accessors! {
     struct GuardUsageBuilder {
+        /// A list of restrictions on which guard may be used.
+        ///
+        /// The default is the empty list.
         pub restrictions: [GuardRestriction],
     }
 }
